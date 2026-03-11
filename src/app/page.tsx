a94f4378d7beca7e2136ae5e4398d3b8728e5459
Jeffrey <yizhoujiang11@gmail.com>
2026-03-11 22:41:45 +0400

    added repo and email link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,30 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
 	return (
 		<main>
 			<h1 className="text-4xl font-bold">Welcome to Zirus!</h1>
 			<p className="mt-4 text-lg">Under active development</p>
+			<div className="flex flex-col gap-2 mt-4">
+				<Link
+					href="https://github.com/JeffreyJYZ/zirus"
+					target="_blank"
+					className="text-blue-500 hover:underline text-2xl"
+				>
+					Repo
+				</Link>
+				<span>
+					Email me at{" "}
+					<Link
+						href="mailto:yizhoujiang11@gmail.com"
+						className="text-blue-500 hover:underline"
+					>
+						yizhoujiang11@gmail.com
+					</Link>{" "}
+					For any questions or suggestions!
+				</span>
+			</div>
 		</main>
 	);
 }
