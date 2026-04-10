7f2638276ec67273ecc7be48e36b484ab6f44100
Jeffrey <yizhoujiang11@gmail.com>
2026-04-10 13:55:47 +0800

    minimized tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,8 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
-	content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./src/**/*.{ts,js,jsx,tsx,mdx}"],
 	theme: {
 		extend: {},
 	},
 	plugins: [],
 };
-
-export default config;
